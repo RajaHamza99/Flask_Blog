abe16abd46e25ea09237c8bc01c0febb861da538
Hamza <rajahamza104@gmail.com>
2020-06-09 15:36:12 +0000

    Flask stage 6

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']=getenv('FLASK_BLOG_URI')
+app.config['SECRET_KEY']=getenv('SECRET_KEY')
 
+db = SQLAlchemy(app)
+bcrypt= Bcrypt(app)
 
 
 from application import routes
